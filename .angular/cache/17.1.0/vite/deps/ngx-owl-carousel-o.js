5ed78e65e3d56a4b1feef4ca81cf7781807cd2fa
Mec33535 <osman33535@gmail.com>
2024-01-22 02:35:24 +0300

    FoodSectionCards

--- .angular/cache/17.1.0/vite/deps/ngx-owl-carousel-o.js
+++ .angular/cache/17.1.0/vite/deps/ngx-owl-carousel-o.js
@@ -1,11 +1,4 @@
 import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from "./chunk-TBSHVKFK.js";
-import {
   ActivatedRoute,
   NavigationEnd,
   Router
@@ -13,6 +6,7 @@ import {
 import "./chunk-TXTMR4R6.js";
 import {
   CommonModule,
+  DOCUMENT,
   LocationStrategy,
   NgClass,
   NgForOf,
@@ -22,6 +16,7 @@ import {
   isPlatformBrowser
 } from "./chunk-NPFINY2H.js";
 import {
+  ANIMATION_MODULE_TYPE,
   Attribute,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
@@ -43,8 +38,11 @@ import {
   Output,
   PLATFORM_ID,
   Renderer2,
+  RendererFactory2,
+  RuntimeError,
   Subject,
   TemplateRef,
+  ViewEncapsulation$1,
   __spreadProps,
   __spreadValues,
   delay,
@@ -52,6 +50,7 @@ import {
   first,
   from,
   fromEvent,
+  inject,
   isDevMode,
   map,
   merge,
@@ -98,6 +97,228 @@ import {
   ɵɵsanitizeUrl,
   ɵɵtemplate
 } from "./chunk-OIWZDSGB.js";
+
+// node_modules/@angular/animations/fesm2022/animations.mjs
+var AnimationMetadataType;
+(function(AnimationMetadataType2) {
+  AnimationMetadataType2[AnimationMetadataType2["State"] = 0] = "State";
+  AnimationMetadataType2[AnimationMetadataType2["Transition"] = 1] = "Transition";
+  AnimationMetadataType2[AnimationMetadataType2["Sequence"] = 2] = "Sequence";
+  AnimationMetadataType2[AnimationMetadataType2["Group"] = 3] = "Group";
+  AnimationMetadataType2[AnimationMetadataType2["Animate"] = 4] = "Animate";
+  AnimationMetadataType2[AnimationMetadataType2["Keyframes"] = 5] = "Keyframes";
+  AnimationMetadataType2[AnimationMetadataType2["Style"] = 6] = "Style";
+  AnimationMetadataType2[AnimationMetadataType2["Trigger"] = 7] = "Trigger";
+  AnimationMetadataType2[AnimationMetadataType2["Reference"] = 8] = "Reference";
+  AnimationMetadataType2[AnimationMetadataType2["AnimateChild"] = 9] = "AnimateChild";
+  AnimationMetadataType2[AnimationMetadataType2["AnimateRef"] = 10] = "AnimateRef";
+  AnimationMetadataType2[AnimationMetadataType2["Query"] = 11] = "Query";
+  AnimationMetadataType2[AnimationMetadataType2["Stagger"] = 12] = "Stagger";
+})(AnimationMetadataType || (AnimationMetadataType = {}));
+function trigger(name, definitions) {
+  return {
+    type: AnimationMetadataType.Trigger,
+    name,
+    definitions,
+    options: {}
+  };
+}
+function animate(timings, styles = null) {
+  return {
+    type: AnimationMetadataType.Animate,
+    styles,
+    timings
+  };
+}
+function sequence(steps, options = null) {
+  return {
+    type: AnimationMetadataType.Sequence,
+    steps,
+    options
+  };
+}
+function style(tokens) {
+  return {
+    type: AnimationMetadataType.Style,
+    styles: tokens,
+    offset: null
+  };
+}
+function state(name, styles, options) {
+  return {
+    type: AnimationMetadataType.State,
+    name,
+    styles,
+    options
+  };
+}
+function transition(stateChangeExpr, steps, options = null) {
+  return {
+    type: AnimationMetadataType.Transition,
+    expr: stateChangeExpr,
+    animation: steps,
+    options
+  };
+}
+var _AnimationBuilder = class _AnimationBuilder {
+};
+_AnimationBuilder.ɵfac = function AnimationBuilder_Factory(t) {
+  return new (t || _AnimationBuilder)();
+};
+_AnimationBuilder.ɵprov = ɵɵdefineInjectable({
+  token: _AnimationBuilder,
+  factory: () => (() => inject(BrowserAnimationBuilder))(),
+  providedIn: "root"
+});
+var AnimationBuilder = _AnimationBuilder;
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(AnimationBuilder, [{
+    type: Injectable,
+    args: [{
+      providedIn: "root",
+      useFactory: () => inject(BrowserAnimationBuilder)
+    }]
+  }], null, null);
+})();
+var AnimationFactory = class {
+};
+var _BrowserAnimationBuilder = class _BrowserAnimationBuilder extends AnimationBuilder {
+  constructor(rootRenderer, doc) {
+    super();
+    this.animationModuleType = inject(ANIMATION_MODULE_TYPE, {
+      optional: true
+    });
+    this._nextAnimationId = 0;
+    const typeData = {
+      id: "0",
+      encapsulation: ViewEncapsulation$1.None,
+      styles: [],
+      data: {
+        animation: []
+      }
+    };
+    this._renderer = rootRenderer.createRenderer(doc.body, typeData);
+    if (this.animationModuleType === null && !isAnimationRenderer(this._renderer)) {
+      throw new RuntimeError(3600, (typeof ngDevMode === "undefined" || ngDevMode) && "Angular detected that the `AnimationBuilder` was injected, but animation support was not enabled. Please make sure that you enable animations in your application by calling `provideAnimations()` or `provideAnimationsAsync()` function.");
+    }
+  }
+  build(animation) {
+    const id = this._nextAnimationId;
+    this._nextAnimationId++;
+    const entry = Array.isArray(animation) ? sequence(animation) : animation;
+    issueAnimationCommand(this._renderer, null, id, "register", [entry]);
+    return new BrowserAnimationFactory(id, this._renderer);
+  }
+};
+_BrowserAnimationBuilder.ɵfac = function BrowserAnimationBuilder_Factory(t) {
+  return new (t || _BrowserAnimationBuilder)(ɵɵinject(RendererFactory2), ɵɵinject(DOCUMENT));
+};
+_BrowserAnimationBuilder.ɵprov = ɵɵdefineInjectable({
+  token: _BrowserAnimationBuilder,
+  factory: _BrowserAnimationBuilder.ɵfac,
+  providedIn: "root"
+});
+var BrowserAnimationBuilder = _BrowserAnimationBuilder;
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(BrowserAnimationBuilder, [{
+    type: Injectable,
+    args: [{
+      providedIn: "root"
+    }]
+  }], () => [{
+    type: RendererFactory2
+  }, {
+    type: Document,
+    decorators: [{
+      type: Inject,
+      args: [DOCUMENT]
+    }]
+  }], null);
+})();
+var BrowserAnimationFactory = class extends AnimationFactory {
+  constructor(_id, _renderer) {
+    super();
+    this._id = _id;
+    this._renderer = _renderer;
+  }
+  create(element, options) {
+    return new RendererAnimationPlayer(this._id, element, options || {}, this._renderer);
+  }
+};
+var RendererAnimationPlayer = class {
+  constructor(id, element, options, _renderer) {
+    this.id = id;
+    this.element = element;
+    this._renderer = _renderer;
+    this.parentPlayer = null;
+    this._started = false;
+    this.totalTime = 0;
+    this._command("create", options);
+  }
+  _listen(eventName, callback) {
+    return this._renderer.listen(this.element, `@@${this.id}:${eventName}`, callback);
+  }
+  _command(command, ...args) {
+    issueAnimationCommand(this._renderer, this.element, this.id, command, args);
+  }
+  onDone(fn) {
+    this._listen("done", fn);
+  }
+  onStart(fn) {
+    this._listen("start", fn);
+  }
+  onDestroy(fn) {
+    this._listen("destroy", fn);
+  }
+  init() {
+    this._command("init");
+  }
+  hasStarted() {
+    return this._started;
+  }
+  play() {
+    this._command("play");
+    this._started = true;
+  }
+  pause() {
+    this._command("pause");
+  }
+  restart() {
+    this._command("restart");
+  }
+  finish() {
+    this._command("finish");
+  }
+  destroy() {
+    this._command("destroy");
+  }
+  reset() {
+    this._command("reset");
+    this._started = false;
+  }
+  setPosition(p) {
+    this._command("setPosition", p);
+  }
+  getPosition() {
+    return unwrapAnimationRenderer(this._renderer)?.engine?.players[this.id]?.getPosition() ?? 0;
+  }
+};
+function issueAnimationCommand(renderer, element, id, command, args) {
+  renderer.setProperty(element, `@@${id}:${command}`, args);
+}
+function unwrapAnimationRenderer(renderer) {
+  const type = renderer.ɵtype;
+  if (type === 0) {
+    return renderer;
+  } else if (type === 1) {
+    return renderer.animationRenderer;
+  }
+  return null;
+}
+function isAnimationRenderer(renderer) {
+  const type = renderer.ɵtype;
+  return type === 0 || type === 1;
+}
 
 // node_modules/ngx-owl-carousel-o/fesm2022/ngx-owl-carousel-o.mjs
 function StageComponent_ng_container_2_2_ng_template_0_Template(rf, ctx) {
@@ -2135,7 +2356,7 @@ var windowProvider = {
   deps: [WindowRef, PLATFORM_ID]
 };
 var WINDOW_PROVIDERS = [browserWindowProvider, windowProvider];
-var DOCUMENT = new InjectionToken("DocumentToken");
+var DOCUMENT2 = new InjectionToken("DocumentToken");
 var DocumentRef = class {
   get nativeDocument() {
     throw new Error("Not implemented.");
@@ -2179,7 +2400,7 @@ var browserDocumentProvider = {
   useClass: BrowserDocumentRef
 };
 var documentProvider = {
-  provide: DOCUMENT,
+  provide: DOCUMENT2,
   useFactory: documentFactory,
   deps: [DocumentRef, PLATFORM_ID]
 };
@@ -2364,7 +2585,7 @@ var AutoplayService = class _AutoplayService {
     }
   }
   static ɵfac = function AutoplayService_Factory(t) {
-    return new (t || _AutoplayService)(ɵɵinject(CarouselService), ɵɵinject(WINDOW), ɵɵinject(DOCUMENT), ɵɵinject(NgZone));
+    return new (t || _AutoplayService)(ɵɵinject(CarouselService), ɵɵinject(WINDOW), ɵɵinject(DOCUMENT2), ɵɵinject(NgZone));
   };
   static ɵprov = ɵɵdefineInjectable({
     token: _AutoplayService,
@@ -2386,7 +2607,7 @@ var AutoplayService = class _AutoplayService {
     type: void 0,
     decorators: [{
       type: Inject,
-      args: [DOCUMENT]
+      args: [DOCUMENT2]
     }]
   }, {
     type: NgZone
@@ -3689,7 +3910,7 @@ var CarouselComponent = class _CarouselComponent {
     this.autoplayService.play();
   }
   static ɵfac = function CarouselComponent_Factory(t) {
-    return new (t || _CarouselComponent)(ɵɵdirectiveInject(ElementRef), ɵɵdirectiveInject(ResizeService), ɵɵdirectiveInject(CarouselService), ɵɵdirectiveInject(NavigationService), ɵɵdirectiveInject(AutoplayService), ɵɵdirectiveInject(LazyLoadService), ɵɵdirectiveInject(AnimateService), ɵɵdirectiveInject(AutoHeightService), ɵɵdirectiveInject(HashService), ɵɵdirectiveInject(OwlLogger), ɵɵdirectiveInject(ChangeDetectorRef), ɵɵdirectiveInject(DOCUMENT));
+    return new (t || _CarouselComponent)(ɵɵdirectiveInject(ElementRef), ɵɵdirectiveInject(ResizeService), ɵɵdirectiveInject(CarouselService), ɵɵdirectiveInject(NavigationService), ɵɵdirectiveInject(AutoplayService), ɵɵdirectiveInject(LazyLoadService), ɵɵdirectiveInject(AnimateService), ɵɵdirectiveInject(AutoHeightService), ɵɵdirectiveInject(HashService), ɵɵdirectiveInject(OwlLogger), ɵɵdirectiveInject(ChangeDetectorRef), ɵɵdirectiveInject(DOCUMENT2));
   };
   static ɵcmp = ɵɵdefineComponent({
     type: _CarouselComponent,
@@ -3817,7 +4038,7 @@ var CarouselComponent = class _CarouselComponent {
     type: void 0,
     decorators: [{
       type: Inject,
-      args: [DOCUMENT]
+      args: [DOCUMENT2]
     }]
   }], {
     slides: [{
@@ -4290,4 +4511,13 @@ export {
   SlideModel,
   SlidesOutputData
 };
+/*! Bundled license information:
+
+@angular/animations/fesm2022/animations.mjs:
+  (**
+   * @license Angular v17.1.0
+   * (c) 2010-2022 Google LLC. https://angular.io/
+   * License: MIT
+   *)
+*/
 //# sourceMappingURL=ngx-owl-carousel-o.js.map
--- src/app/food-section/food-section.component.ts
+++ src/app/food-section/food-section.component.ts
@@ -1,12 +1,125 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CartSvgComponent } from '../cart-svg/cart-svg.component';
+import { IsotopeOptions } from 'isotope-layout';
 
 @Component({
   selector: 'app-food-section',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    CartSvgComponent,
+  ],
   templateUrl: './food-section.component.html',
   styleUrl: './food-section.component.scss'
 })
 export class FoodSectionComponent {
+
+  filterFunction(item: any): boolean {
+    return item.title === "Angular";
+  }
+
+  IsotopeModule: IsotopeOptions = {
+    // const data = this.
+    filter: 'data',
+    itemSelector: 'item-selector',
+    layoutMode: 'cellsByRow',
+    sortBy: '3s',
+  }
+
+  FilterMenu = [
+    {
+      id: 'filter-1',
+      dataFilter: "*",
+      filterText: "All",
+    },
+    {
+      id: 'filter-2',
+      dataFilter: ".burger",
+      filterText: "Buger",
+    },
+    {
+      id: 'filter-3',
+      dataFilter: ".pizza",
+      filterText: "Pizza",
+    },
+    {
+      id: 'filter-4',
+      dataFilter: ".pasta",
+      filterText: "Pasta",
+    },
+    {
+      id: 'filter-5',
+      dataFilter: ".fries",
+      filterText: "Fries",
+    }
+  ]
+  MenuCards = [
+    {
+      id: 'menu-card-1',
+      menuImg: '../assets/f1.png',
+      menuTitle: "Delicious Pizza",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '20$'
+    }, {
+      id: 'menu-card-2',
+      menuImg: "../assets/f2.png",
+      menuTitle: "Tasty Burger",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '15$'
+    }, {
+      id: 'menu-card-3',
+      menuImg: "../assets/f3.png",
+      menuTitle: "Delicious Pizza",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '17$'
+    }, {
+      id: 'menu-card-4',
+      menuImg: "../assets/f4.png",
+      menuTitle: "Delicious Pasta",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '18$'
+    }, {
+      id: 'menu-card-5',
+      menuImg: "../assets/f5.png",
+      menuTitle: "French Fries",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '10$'
+    }, {
+      id: 'menu-card-6',
+      menuImg: "../assets/f6.png",
+      menuTitle: "Delicious Pizza",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '15$'
+    }, {
+      id: 'menu-card-7',
+      menuImg: "../assets/f7.png",
+      menuTitle: "Tasty Burger",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '12$'
+    }, {
+      id: 'menu-card-8',
+      menuImg: "../assets/f8.png",
+      menuTitle: "Tasty Burger",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '14$'
+    }, {
+      id: 'menu-card-9',
+      menuImg: "../assets/f9.png",
+      menuTitle: "Delicious Pasta",
+      menuText: `Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus
+      sed eaque`,
+      menuPrice: '10$'
+    },
+  ]
 
 }
